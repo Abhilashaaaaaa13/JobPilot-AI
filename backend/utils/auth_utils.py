7c33da6e13a0040e658aad3a834c694a0e3f835c
Abhilashaaaaaa13 <abhilasha1333@gmail.com>
2026-03-16 23:29:25 +0530

    added utils

---
+++ backend/utils/auth_utils.py
@@ -0,0 +1,38 @@
+# backend/utils/auth_utils.py
+# Password hashing + JWT token logic
+
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from backend.config import SECRET_KEY, ALGORITHM , TOKEN_EXPIRE_MIN
+
+# Bcrypt context
+# Why bcrypt?
+# → One way hash — original password recover nahi hota
+# → Slow by design — brute force difficult
+# → Industry standard
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password:str)->str:
+    return pwd_context.hash(password)
+
+def verify_password(plain: str, hashed:str)->bool:
+    return pwd_context.verify(plain,hashed)
+
+def create_access_token(data:dict)->str:
+    """ Create JWT token.
+    Data has user_id and email.
+    Token will expire after TOKEN_EXPIRE_MIN """
+    to_encode = data.copy()
+    expire = datetime.utcnow()+timedelta(minutes=TOKEN_EXPIRE_MIN)
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_token(token:str)->dict:
+    """Decode Token.
+    Invalid or expire -> exception raise"""
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
